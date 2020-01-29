11d6433454d602c54fdd59f9900fddceb6c57765
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 07:18:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function theBeatlesPlay(musicians,instruments) {
 function johnLennonFacts(array) {
   var results = [];
   var count = 0;
-  
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   while (count < facts.length){
     results.push(facts[count] + "!!!");
     count++;
