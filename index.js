40cc1f8e5de30ff607bd194b0db7968bfde4db69
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 06:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 
 Function theBeatlesPlay(musicians, instruments){
-  var array = [];
+  var Array = [];
  for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + 'plays' + instruments[i])
  }
