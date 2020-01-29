175389a2631982041a33889013aad079de392d0c
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 06:20:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // add solution here
 
-Function theBeatlesPlay(musicians,instruments){
-  var Array = [];
-  for (var i = 0; i < musicians + 'plays' + instruments [i]);
+Function theBeatlesPlay(musicians, instruments){
+  var array = [];
+ for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + 'plays' + instruments[i])
+ }
 }
-
 return Array;
