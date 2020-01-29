76c9215cbacc58d60ef2510ae370d5a9dcc4caf5
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 06:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-Function theBeatlesPlay(musicians\, instruments) {
-  var array = ();
+Function theBeatlesPlay(musicians, instruments) {
+  var array = [];
  for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + 'plays' + instruments[i]);
  }
@@ -7,12 +7,6 @@ Function theBeatlesPlay(musicians\, instruments) {
 }
 
 Function johnLennonFacts(facts) {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
   var results = [];
   var count = 0;
   while (count < facts.length){
