3e54a08d7eabf8ae7db53c22b964846b6fb06dd1
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 06:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ Function theBeatlesPlay(musicians, instruments){
   var Array = [];
  for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + 'plays' + instruments[i])
- }return Array;
+ }
+ return Array;
 }
