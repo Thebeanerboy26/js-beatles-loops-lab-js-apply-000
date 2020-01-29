9f996c1e490de9892af69856bc3717e1d43fa445
Josue Viramontes <josueviramontes1997@gmail.com>
2020-01-29 07:08:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-function theBeatlesPlay(musicians,instruments) {
-  var x = []
-  for (var i = 0; i < musicians.length; i++) {
-    x.push(musicians[i] + ' plays ' + instruments[i])
-  }
-  return x;
+Function theBeatlesPlay(musicians,instruments) {
+  var array = [];
+ for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + 'plays' + instruments[i]);
+ }
+ return array;
 }
 
-function johnLennonFacts(array) {
-  var i = 0
-  while (i < array.length) {
-    array[i] += '!!!';
-    i++;
+Function johnLennonFacts(array) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length){
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return array;
+  return results;
 }
 
-function iLoveTheBeatles(number) {
-  var arr = [];
-  var i = 0;
-  do {
-    arr.push('I love the Beatles!');
-    i++;
-  } while (i <= number && number < 15);
+Function iLoveTheBeatles(num) {
+  var array = [];
+  
+  do{
+    array.push("I love the Beatles!")
+    num++;
+  } 
+  while (num < 15);
   return array;
 }
